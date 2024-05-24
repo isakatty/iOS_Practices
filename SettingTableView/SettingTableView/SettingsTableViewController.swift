8784d1e569d58044c4686239916547908adfa693
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-05-24 09:28:56 +0900

    [Fix] TableView Dynamic content 수정

--- SettingTableView/SettingTableView/SettingsTableViewController.swift
+++ SettingTableView/SettingTableView/SettingsTableViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 class SettingsTableViewController: UITableViewController {
     
     let tryData: [String] = [
-        "전체 설정","공지사항","실험실","버전정보","개인 설정","개인/보안","알림","채팅","멀티프로필","기타","고객센터/도움말"
+        "공지사항","실험실","버전정보","개인/보안","알림","채팅","멀티프로필","고객센터/도움말"
     ]
-
+    // "전체 설정" "개인 설정" "기타"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -35,13 +36,8 @@ class SettingsTableViewController: UITableViewController {
         
         cell.backgroundColor = .black
         cell.textLabel?.text = tryData[indexPath.row]
-        if indexPath.row == 0 || indexPath.row == 4 || indexPath.row == 9 {
-            cell.textLabel?.textColor = .lightGray
-            cell.textLabel?.font = .systemFont(ofSize: 15, weight: .bold)
-        } else {
-            cell.textLabel?.textColor = .white
-            cell.textLabel?.font = .systemFont(ofSize: 13, weight: .light)
-        }
+        cell.textLabel?.textColor = .white
+        cell.textLabel?.font = .systemFont(ofSize: 13, weight: .light)
         
         return cell
     }
@@ -53,7 +49,24 @@ class SettingsTableViewController: UITableViewController {
         return 60
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
     
-
-
+    override func tableView(
+        _ tableView: UITableView,
+        titleForHeaderInSection section: Int
+    ) -> String? {
+        if section == 0 {
+            return "전체 설정"
+        } else if section == 1 {
+            return "즐겨찾기"
+        } else if section == 2 {
+            return "기타"
+        } else {
+            return "섹션"
+        }
+    }
+    
+    
 }
