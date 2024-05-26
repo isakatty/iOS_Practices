2f375cb457ddd8f17396b821884c02cf75c498f5
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-05-27 00:30:41 +0900

    [UI] #11 #12 tableview UI 및 cell 오토레이아웃 설정

---
+++ SettingTableView/SettingTableView/ShoppingTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ShoppingTableViewCell.swift
+//  SettingTableView
+//
+//  Created by Jisoo Ham on 5/27/24.
+//
+
+import UIKit
+
+class ShoppingTableViewCell: UITableViewCell {
+
+}
---
+++ SettingTableView/SettingTableView/ShoppingTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ShoppingTableViewController.swift
+//  SettingTableView
+//
+//  Created by Jisoo Ham on 5/27/24.
+//
+
+import UIKit
+
+class ShoppingTableViewController: UITableViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
