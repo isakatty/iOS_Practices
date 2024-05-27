96909660f9619313ea64f7f2a383f5e02e47ab64
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-05-27 16:13:12 +0900

    [UI] #17 UI component 선언

--- TravelMagazineUIKit/TravelMagazineUIKit/AdTableViewCell.swift
+++ TravelMagazineUIKit/TravelMagazineUIKit/AdTableViewCell.swift
@@ -8,16 +8,8 @@
 import UIKit
 
 class AdTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    @IBOutlet var adLabel: UILabel!
+    
+    
     
 }
--- TravelMagazineUIKit/TravelMagazineUIKit/CityDetailTableViewCell.swift
+++ TravelMagazineUIKit/TravelMagazineUIKit/CityDetailTableViewCell.swift
@@ -9,5 +9,13 @@ import UIKit
 
 class CityDetailTableViewCell: UITableViewCell {
 
+    @IBOutlet var cityNameLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var gradeLabel: UILabel!
+    
+    @IBOutlet var favBtn: UIButton!
+    @IBOutlet var mainImgView: UIImageView!
+    
+    
     
 }
