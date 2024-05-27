6456a45b32822306d9408c5dceedd1fcac504b78
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-05-27 16:12:52 +0900

    [UI] #17 Tabbar 적용, custom cell autolayout 적용

---
+++ TravelMagazineUIKit/TravelMagazineUIKit/CityDetailTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CityDetailTableViewCell.swift
+//  TravelMagazineUIKit
+//
+//  Created by Jisoo HAM on 5/27/24.
+//
+
+import UIKit
+
+class CityDetailTableViewCell: UITableViewCell {
+
+    
+}
