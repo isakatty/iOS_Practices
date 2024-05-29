3822576f5b2150f91bd274b7d8656ba55939848a
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-05-30 00:12:31 +0900

    [Feat] #23 searchBar를 통해 단순 한글 검색 기능 추가

--- TravelMagazineUIKit/TravelMagazineUIKit/ViewController/PopularCityViewController.swift
+++ TravelMagazineUIKit/TravelMagazineUIKit/ViewController/PopularCityViewController.swift
@@ -17,7 +17,6 @@ import Kingfisher
  - sender.selectedSegmentIndex 통해 각각의 seg에 접근
     - switch case 문으로 index 분류 / 분류에 맞는 데이터 할당 / tableView.reload()
     - 더 멋진 방법 없나 . . ?
- 
  */
 
 class PopularCityViewController: UIViewController {
@@ -37,6 +36,7 @@ class PopularCityViewController: UIViewController {
         
         configureTableView()
         configureSegmented()
+        configureSearchBar()
         
         hideKeyboard()
     }
@@ -64,7 +64,11 @@ class PopularCityViewController: UIViewController {
             segments.setTitle(segName[compo], forSegmentAt: compo)
         }
     }
-    
+    func configureSearchBar() {
+        searchBar.delegate = self
+        
+        searchBar.placeholder = "인기 도시를 검색해보세요 !"
+    }
     
     @IBAction func segmentSelected(_ sender: UISegmentedControl) {
         print(sender.selectedSegmentIndex)
@@ -89,6 +93,26 @@ class PopularCityViewController: UIViewController {
         
     }
     
+}
+
+/* TODO: SearchBar
+ - enterTap -> 검색 : UISearchBarDelegate를 통해 구현된 함수 사용
+ - 실시간 검색 기능 : text에 변동이 있으면 검색 로직
+ Ex) segment 국내 -> 서치바 검색 : 분류해서 검색
+   1. segments.selectedSegmentIndex 를 통해 seg index 접근 후 데이터 필터링
+   2. searchbar.text 필터링
+    ---> 데이터 보여주기
+ */
+extension PopularCityViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let text = searchBar.text, !text.isEmpty 
+        else { return }
+        
+        filteredCity = cityList.filter({ $0.city_name.contains(text) || $0.city_english_name.contains(text) || $0.city_explain.contains(text)
+        })
+        
+        tableView.reloadData()
+    }
     
     
 }
