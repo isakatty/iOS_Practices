2680981c3589c92ffd204e1d0704e161fe52a376
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-05-31 01:13:05 +0900

    [Refactor] #29 annotation 생성 로직 수정 및 메소드 수정

--- TravelMagazineUIKit/TravelMagazineUIKit/ViewController/RestaurantMapViewController.swift
+++ TravelMagazineUIKit/TravelMagazineUIKit/ViewController/RestaurantMapViewController.swift
@@ -16,6 +16,9 @@ import MapKit
     -> 위치 설정 및 몇 미터 반경까지 보여줄지 설정 ⭐️
     -> annotaion 추가
  3. segments 필터
+    MARK: ⚠️ 왤까 !!!!
+    -> 필터링을 통해 핀을 다시 찍을 때, 일반 tableView.reloadData() 하듯 생성하는 함수를 다시 호출하면 안되는 이유는 뭘까 ?
+        남아있을 수 있기 때문에 명시적으로 지우고 다시 추가해주어야한다 ? ?
  */
 
 enum FoodCategory: Int {
@@ -55,11 +58,21 @@ class RestaurantMapViewController: UIViewController {
         
         filteredData = restaurants
         
-        configureMapView(stores: filteredData)
+        // MARK: 함수를 계속 호출하게 되는데, 매번 생성하는 것보다 한번 생성해놓는게 더 효율적이지 않은가 ?
+        let coordinate = CLLocationCoordinate2D(
+            latitude: restaurants[3].latitude,
+            longitude: restaurants[3].longitude
+        )
+        
+        mapView.region = MKCoordinateRegion(
+            center: coordinate,
+            latitudinalMeters: 2000,
+            longitudinalMeters: 2000
+        )
+        
+        configureMapAnnotaion(stores: filteredData)
         configureSeg()
         
-        
-//        filterFoodCategoryName()
     }
     
     
@@ -84,26 +97,9 @@ class RestaurantMapViewController: UIViewController {
 //            filteredData = restaurants
 //        }
         
-        configureMapView(stores: filteredData)
+        configureMapAnnotaion(stores: filteredData)
     }
     
-    
-    func configureMapView(stores: [Restaurant]) {
-        mapView.removeAnnotations(mapView.annotations)
-        
-        let coordinate = CLLocationCoordinate2D(
-            latitude: restaurants[3].latitude,
-            longitude: restaurants[3].longitude
-        )
-        
-        mapView.region = MKCoordinateRegion(
-            center: coordinate,
-            latitudinalMeters: 1500,
-            longitudinalMeters: 5000
-        )
-        
-        configureMapAnnotaion(stores: stores)
-    }
     func configureSeg() {
 //        for index in segName.indices {
 //            segments.setTitle(segName[index], forSegmentAt: index)
@@ -113,16 +109,30 @@ class RestaurantMapViewController: UIViewController {
         }
     }
     func configureMapAnnotaion(stores: [Restaurant]) {
+        // 1. 혹시라도 있을 annotaion 제거
+        mapView.removeAnnotations(mapView.annotations)
+        
+        // 2. 생성한 annotions 배열 빈배열 만들기
         annotations.removeAll()
         
         // index를 돌면서 핀 만들기
-        for index in stores.indices {
+//        for index in stores.indices {
+//            let annotation = MKPointAnnotation()
+//            annotation.coordinate = CLLocationCoordinate2D(
+//                latitude: stores[index].latitude,
+//                longitude: stores[index].longitude
+//            )
+//            annotation.title = stores[index].name
+//            annotations.append(annotation)
+//        }
+        
+        for (_, store) in stores.enumerated() {
             let annotation = MKPointAnnotation()
             annotation.coordinate = CLLocationCoordinate2D(
-                latitude: stores[index].latitude,
-                longitude: stores[index].longitude
+                latitude: store.latitude,
+                longitude: store.longitude
             )
-            annotation.title = stores[index].name
+            annotation.title = store.name
             annotations.append(annotation)
         }
         
