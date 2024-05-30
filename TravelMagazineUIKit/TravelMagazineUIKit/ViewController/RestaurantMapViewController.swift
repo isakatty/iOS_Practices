c8541e51b758a1e33737718d33ab5d3396e060a9
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-05-30 17:50:33 +0900

    [Setting] #29 식당의 위치, mapview 사용 셋팅

---
+++ TravelMagazineUIKit/TravelMagazineUIKit/ViewController/RestaurantMapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RestaurantMapViewController.swift
+//  TravelMagazineUIKit
+//
+//  Created by Jisoo Ham on 5/30/24.
+//
+
+import UIKit
+
+class RestaurantMapViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
