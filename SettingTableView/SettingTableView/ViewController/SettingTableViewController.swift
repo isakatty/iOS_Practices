1c7f165354767b2fcba44d2a440ec27ec7a149eb
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-06-03 17:10:07 +0900

    Fix/#37 Enum을 이용하여 설정 화면 구현 (#38)
    * [Fix] #37 배열 아닌 enum으로 처리
    
    * [Fix] #37 viewController 연결 수정

--- SettingTableView/SettingTableView/ViewController/SettingTableViewController.swift
+++ SettingTableView/SettingTableView/ViewController/SettingTableViewController.swift
@@ -7,33 +7,48 @@
 
 import UIKit
 
-/*  
- MARK: section의 textColor 변경은 어디서 해야하지 ?
- 
- 데이터 구성 이런 형식으로 되면 안되는거였던건가 ?
- 
- let tryData: [String] = [
-     "전체 설정","공지사항","실험실","버전정보","개인 설정","개인/보안","알림","채팅","멀티프로필","기타","고객센터/도움말"
- ]
- 
- 조건문 통해서 최대한 UI 비슷하게 만든건 SettingsTableViewController
- */
+enum SettingOptions: Int, CaseIterable {
+    case total, personal, others
+    
+    var mainOptions: String {
+        switch self {
+        case .total:
+            return "전체 설정"
+        case .personal:
+            return "개인 설정"
+        case .others:
+            return "기타"
+        }
+    }
+    
+    var subOptions: [String] {
+        switch self {
+        case .total:
+            return ["공지사항", "실험실", "버전정보"]
+        case .personal:
+            return ["개인/보안", "알림", "채팅", "멀티 프로필"]
+        case .others:
+            return ["고객센터/도움말"]
+        }
+    }
+}
+
+
 class SettingTableViewController: UITableViewController {
     
-    let data: [Setting] = [
-        Setting(section: "전체 설정", row: ["공지사항", "실험실", "버전정보"]),
-        Setting(section: "개인 설정", row: ["개인/보안", "알림", "채팅", "멀티 프로필"]),
-        Setting(section: "기타", row: ["고객센터/도움말"])
-    ]
+//    let data: [Setting] = [
+//        Setting(section: "전체 설정", row: ["공지사항", "실험실", "버전정보"]),
+//        Setting(section: "개인 설정", row: ["개인/보안", "알림", "채팅", "멀티 프로필"]),
+//        Setting(section: "기타", row: ["고객센터/도움말"])
+//    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setTableViewlBackground()
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return data.count
+        return SettingOptions.allCases.count
     }
     
     
@@ -41,14 +56,14 @@ class SettingTableViewController: UITableViewController {
         _ tableView: UITableView,
         numberOfRowsInSection section: Int
     ) -> Int {
-        return data[section].row.count
+        return SettingOptions.allCases[section].subOptions.count
     }
     
     override func tableView(
         _ tableView: UITableView
         , heightForRowAt indexPath: IndexPath
     ) -> CGFloat {
-        return 60
+        return 120
     }
     
     override func tableView(
@@ -58,7 +73,8 @@ class SettingTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SettingCell")!
         
         cell.backgroundColor = .black
-        cell.textLabel?.text = data[indexPath.section].row[indexPath.row]
+        cell.textLabel?.text =
+        SettingOptions.allCases[indexPath.section].subOptions[indexPath.row]
         cell.textLabel?.textColor = .white
         cell.textLabel?.font = .systemFont(ofSize: 13)
         
@@ -70,24 +86,10 @@ class SettingTableViewController: UITableViewController {
         titleForHeaderInSection section: Int
     ) -> String? {
         
-        let text = data[section].section
+        let text = SettingOptions.allCases[section].mainOptions
         return text
     }
     
-    override func tableView(
-        _ tableView: UITableView,
-        titleForFooterInSection section: Int
-    ) -> String? {
-        return ""
-    }
-    
-}
-
-extension UITableViewController {
-    func setTableViewlBackground() {
-        view.backgroundColor = .black
-        tableView.backgroundColor = .black
-    }
 }
 
 struct Setting {
