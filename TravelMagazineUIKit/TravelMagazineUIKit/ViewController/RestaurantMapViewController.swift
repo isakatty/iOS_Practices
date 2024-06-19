3b916e267779993cf15bf6b552cfe0a3da24e1a5
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-06-20 00:31:04 +0900

    [Feat] 위치권한 설정과 맵뷰에 사용자 위치 보여주기

--- TravelMagazineUIKit/TravelMagazineUIKit/ViewController/RestaurantMapViewController.swift
+++ TravelMagazineUIKit/TravelMagazineUIKit/ViewController/RestaurantMapViewController.swift
@@ -7,19 +7,8 @@
 
 import UIKit
 
+import CoreLocation
 import MapKit
-
-
-/* TODO: 지도에 레스토랑 핀 찍기 / segments적용
- 1. 데이터 주입
- 2. 레스토랑 핀찍기 - VC 진입시 전체 어노테이션 적용
-    -> 위치 설정 및 몇 미터 반경까지 보여줄지 설정 ⭐️
-    -> annotaion 추가
- 3. segments 필터
-    MARK: ⚠️ 왤까 !!!!
-    -> 필터링을 통해 핀을 다시 찍을 때, 일반 tableView.reloadData() 하듯 생성하는 함수를 다시 호출하면 안되는 이유는 뭘까 ?
-        남아있을 수 있기 때문에 명시적으로 지우고 다시 추가해주어야한다 ? ?
- */
 
 enum FoodCategory: Int { // rawValue를 string 값으로 변경해서 적용하는것
     case all = 0
@@ -51,27 +40,20 @@ class RestaurantMapViewController: UIViewController {
     var foodCategory = [String]()
     let segName: [String] = ["전체", "한식", "중식"]
     
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationItem.title = "레스토랑 지도"
+        locationManager.delegate = self
         
         filteredData = restaurants
         
-        // MARK: 함수를 계속 호출하게 되는데, 매번 생성하는 것보다 한번 생성해놓는게 더 효율적이지 않은가 ?
-        let coordinate = CLLocationCoordinate2D(
-            latitude: restaurants[3].latitude,
-            longitude: restaurants[3].longitude
-        )
-        
-        mapView.region = MKCoordinateRegion(
-            center: coordinate,
-            latitudinalMeters: 2000,
-            longitudinalMeters: 2000
-        )
         
         configureMapAnnotaion(stores: filteredData)
         configureSeg()
+        checkLocationAuthorization()
         
     }
     
@@ -110,8 +92,6 @@ class RestaurantMapViewController: UIViewController {
         mapView.addAnnotations(annotations)
     }
     
-    // MARK: 모든 카테고리를 segments로 표현하기엔 HIG에서 벗어남.
-    // 전체, 한식, 중식만
     func filterFoodCategoryName() {
         // restaurantArray의 category 가져다가 중복 제거
         for (_, store) in restaurants.enumerated() {
@@ -120,5 +100,125 @@ class RestaurantMapViewController: UIViewController {
         foodCategory = Array(Set(foodCategory))
         
     }
+    
+    func checkLocationAuthorization() {
+        // iphone 위치 설정 확인
+        if CLLocationManager.locationServicesEnabled() {
+            // device 위치 설정 확인
+            checkDeviceLocationAuthorization()
+        } else {
+            // 설정으로 보내기
+            UIApplication.shared.open(
+                URL(
+                    string: UIApplication.openSettingsURLString
+                )!,
+                options: [:],
+                completionHandler: nil
+            )
+        }
+    }
+    
+    func checkDeviceLocationAuthorization() {
+        
+        var status: CLAuthorizationStatus
+        
+        if #available(iOS 14.0, *) {
+            status = locationManager.authorizationStatus
+        } else {
+            status = CLLocationManager.authorizationStatus()
+        }
+        
+        switch status {
+        case .notDetermined:
+            // alert 띄우기
+            print("위치정보 허용 관련 설정 아무것도 안한 상태")
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            // 위치권한 요청
+            locationManager.requestWhenInUseAuthorization()
+        case .denied:
+            print("거부")
+            
+            let coordinate = CLLocationCoordinate2D(
+                latitude: restaurants[3].latitude,
+                longitude: restaurants[3].longitude
+            )
+            
+            setRegionAndAnnotaion(center: coordinate)
+            
+            let alert = UIAlertController(
+                title: "위치정보 허용 거부",
+                message: "위치 권한 허용을 허용해야 서비스 이용이 원활합니다.",
+                preferredStyle: .alert
+            )
+            
+            let alertAction = UIAlertAction(title: "확인", style: .destructive) { _ in
+                UIApplication.shared.open(
+                    URL(
+                        string: UIApplication.openSettingsURLString
+                    )!,
+                    options: [:],
+                    completionHandler: nil
+                )
+            }
+            alert.addAction(alertAction)
+            
+            present(alert, animated: true)
+            
+        case .authorizedWhenInUse:
+            print("앱 사용할 때만")
+            
+            locationManager.startUpdatingLocation()
+        default :
+            print("빙글")
+        }
+        
+    }
+    
+    func setRegionAndAnnotaion(center: CLLocationCoordinate2D) {
+        let region = MKCoordinateRegion(
+            center: center,
+            latitudinalMeters: 500,
+            longitudinalMeters: 500
+        )
+        
+        mapView.setRegion(region, animated: true)
+    }
 }
 
+extension RestaurantMapViewController: CLLocationManagerDelegate {
+    
+    // 위치 권한 허용시 위치정보를 업데이트함
+    func locationManager(
+        _ manager: CLLocationManager,
+        didUpdateLocations locations: [CLLocation]
+    ) {
+        print(#function)
+        // 실제 위치를 받아서 맵뷰에 반영 가능한 곳
+        if let location = locations.last {
+            print(location)
+            // 37.46648600,+127.02836200
+            setRegionAndAnnotaion(center: location.coordinate)
+        }
+    }
+    
+    
+    // 위치정보 못가져왔을 때
+    func locationManager(
+        _ manager: CLLocationManager,
+        didFailWithError error: any Error
+    ) {
+        print(error)
+    }
+    
+    // 권한이 바꼈을 때 - iOS 14+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
+    }
+    // 권한이 바꼈을 때 - iOS 14-
+    func locationManager(
+        _ manager: CLLocationManager,
+        didChangeAuthorization status: CLAuthorizationStatus
+    ) {
+        checkLocationAuthorization()
+    }
+}
